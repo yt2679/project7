042afed14cf507454e0ef2443381e08da50dd6b5
Denis Aleksandrov <da2898@columbia.edu>
2019-11-30 01:54:20 +0000

    Define Sighting model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Sighting(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    unique_squirrel_id = models.CharField(max_length=100)
+    shift = models.CharField(max_length=20)
+    date = models.DateField()
+    age = models.CharField(max_length=20)
+    primary_fur_color = models.CharField(max_length=200)
+    location  = models.CharField(max_length=200)
+    specific_location  = models.CharField(max_length=200)
+    running = models.BooleanField()
+    chasing = models.BooleanField()
+    climbing = models.BooleanField()
+    eating = models.BooleanField()
+    foraging = models.BooleanField()
+    other_activities = models.CharField(max_length=200)
+    kuks = models.BooleanField()
+    quaas = models.BooleanField()
+    moans = models.BooleanField()
+    tail_flags = models.BooleanField()
+    tail_twitches = models.BooleanField()
+    approaches = models.BooleanField()
+    indifferent = models.BooleanField()
+    runs_from = models.BooleanField()
+
+
