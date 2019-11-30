fb9428d989ceed15edd5448e065cc35ac460772c
ytiwab <ytiwab@gmail.com>
2019-11-30 04:15:07 +0000

    Add null=True in some Sighting attributes

--- tracker/management/commands/import_squirrel_data.py
+++ tracker/management/commands/import_squirrel_data.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 import pandas as pd
+from tracker.models import Sighting
 
 class Command(BaseCommand):
     help = 'imports squirrel data form path'
@@ -10,4 +11,29 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         path = kwargs['path']
         df = pd.read_csv(path)
-        print(df)
+        for index, row in df.iterrows():
+            sighting = Sighting()
+            sighting.latitude = row['X']
+            sighting.longitude = row['Y']
+            sighting.unique_squirrel_id = row['Unique Squirrel ID']
+            sighting.shift = row['Shift']
+            sighting.date = row['Date']
+            sighting.age = row['Age']
+            sighting.primary_fur_color = row['Primary Fur Color']
+            sighting.location = row['Location']
+            sighting.specific_location = row['Specific Location']
+            sighting.running = row['Running']
+            sighting.chasing = row['Chasing']
+            sighting.climbing = row['Climbing']
+            sighting.eating = row['Eating']
+            sighting.foraging = row['Foraging']
+            sighting.other_activities = row['Other Activities']
+            sighting.kuks = row['Kuks']
+            sighting.quaas = row['Quaas']
+            sighting.moans = row['Moans']
+            sighting.tail_flags = row['Tail flags']
+            sighting.tail_twitches = row['Tail twitches']
+            sighting.approaches = row['Approaches']
+            sighting.indifferent = row['Indifferent']
+            sighting.runs_from = row['Runs from']
+            sighting.save()
