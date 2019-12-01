0ec35b768b24f0dc6dd4586d2351438d44a84a6e
Denis Aleksandrov <da2898@columbia.edu>
2019-12-01 22:16:35 +0000

    Add sightings view

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Sighting
+
+
+
 
 # Create your views here.
+
+def sightings(request):
+    all_squirrels = Sighting.objects.all()
+    return render(request, 'tracker/sightings.html', locals())
+
+
