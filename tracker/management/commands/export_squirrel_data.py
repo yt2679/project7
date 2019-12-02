863b325c86b8f74d5cab06d49979fc8ae3ecab83
ytiwab <ytiwab@gmail.com>
2019-12-02 03:37:36 +0000

    Write complete export

---
+++ tracker/management/commands/export_squirrel_data.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from tracker.models import Sighting
+import csv
+
+class Command(BaseCommand):
+    help = 'exports squirrel data to path'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
+    
+    def handle(self, *args, **kwargs):
+        path = kwargs['path']
+        sightings = Sighting.objects.all()
+        fields = [field.name for field in Sighting._meta.fields]
+        with open(path, mode='w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            for sighting in sightings:
+                writer.writerow([getattr(sighting, field) for field in fields])
+                
